4d038df4a19db8c2646ddc7178d0c088b7ac03bc
jaciel7 <162911580+jaciel7@users.noreply.github.com>
2024-10-30 22:30:27 -0600

    SegundaConfiguracion

---
+++ 06_Texturing/06_Texturing.cpp
@@ -0,0 +1,283 @@
+/*
+* 
+* 06 - Texturizado
+*/
+
+#include <iostream>
+#include <stdlib.h>
+
+// GLAD: Multi-Language GL/GLES/EGL/GLX/WGL Loader-Generator
+// https://glad.dav1d.de/
+#include <glad/glad.h>
+
+// GLFW: https://www.glfw.org/
+#include <GLFW/glfw3.h>
+
+// GLM: OpenGL Math library
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+
+// Model loading classes
+#include <shader_m.h>
+#include <camera.h>
+#include <model.h>
+#include <iostream>
+
+// Functions
+bool Start();
+bool Update();
+
+// Definición de callbacks
+void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+void mouse_callback(GLFWwindow* window, double xpos, double ypos);
+void scroll_callback(GLFWwindow* window, double xoffset, double yoffset);
+void processInput(GLFWwindow *window);
+
+// Gobals
+GLFWwindow* window;
+
+// Tamaño en pixeles de la ventana
+const unsigned int SCR_WIDTH = 1024;
+const unsigned int SCR_HEIGHT = 768;
+
+// Definición de cámara (posición en XYZ)
+Camera camera(glm::vec3(0.0f, 2.0f, 10.0f));
+
+// Controladores para el movimiento del mouse
+float lastX = SCR_WIDTH / 2.0f;
+float lastY = SCR_HEIGHT / 2.0f;
+bool firstMouse = true;
+
+// Variables para la velocidad de reproducción
+// de la animación
+float deltaTime = 0.0f;
+float lastFrame = 0.0f;
+
+// Shaders
+Shader *staticShader;
+
+// Carga la información del modelo
+Model   *object;
+Model	*cubeenv;
+
+// Entrada a función principal
+int main()
+{
+	if (!Start())
+		return -1;
+
+	/* Loop until the user closes the window */
+	while (!glfwWindowShouldClose(window))
+	{
+		if (!Update())
+			break;
+	}
+
+	glfwTerminate();
+	return 0;
+
+	// Loop de renderizado
+	while (!glfwWindowShouldClose(window))
+	{
+		
+	}
+
+	// glfw: Terminamos el programa y liberamos memoria
+	glfwTerminate();
+	return 0;
+}
+
+bool Start() {
+	// Inicialización de GLFW
+
+	glfwInit();
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+	// Creación de la ventana con GLFW
+	window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "Texturing", NULL, NULL);
+	if (window == NULL)
+	{
+		std::cout << "Failed to create GLFW window" << std::endl;
+		glfwTerminate();
+		return false;
+	}
+	glfwMakeContextCurrent(window);
+	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+	glfwSetCursorPosCallback(window, mouse_callback);
+	glfwSetScrollCallback(window, scroll_callback);
+
+	// Ocultar el cursor mientras se rota la escena
+	// glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
+
+	// glad: Cargar todos los apuntadores
+	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+	{
+		std::cout << "Failed to initialize GLAD" << std::endl;
+		return false;
+	}
+
+	// Activación de buffer de profundidad
+	glEnable(GL_DEPTH_TEST);
+
+	// Compilación y enlace de shaders
+	staticShader = new Shader("shaders/10_vertex_simple.vs", "shaders/10_fragment_simple.fs");
+
+	// Dibujar en malla de alambre
+	// glPolygonMode(GL_FRONT_AND_BACK, GL_POINT);
+
+	// Actividad 1: Texturizar modelo de casa y mesa e importar los modelos en OpenGL
+	object = new Model("models/vanus.fbx");
+
+	
+	// Actividad 2: Texturizar modelo del cubo de fondo para crear un
+	// efecto de textura de ambiente
+	cubeenv = new Model("models/mycube.fbx");
+
+	return true;
+}
+
+bool Update() {
+	// Cálculo del framerate
+	float currentFrame = (float)glfwGetTime();
+	deltaTime = currentFrame - lastFrame;
+	lastFrame = currentFrame;
+
+	// Procesa la entrada del teclado o mouse
+	processInput(window);
+
+	// Renderizado R - G - B - A
+	glClearColor(1.0f, 1.0f, 1.0f, 0.0f);
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+	glm::mat4 projection = glm::perspective(glm::radians(camera.Zoom), (float)SCR_WIDTH / (float)SCR_HEIGHT, 0.1f, 10000.0f);
+	glm::mat4 view = camera.GetViewMatrix();
+
+	// objeto estático (modelo)
+	{
+		// Activamos el shader del objeto a renderizar
+		staticShader->use();
+
+		// Activamos para objetos transparentes
+		glEnable(GL_BLEND);
+		glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+
+		// Aplicamos transformaciones de proyección y cámara (si las hubiera)
+		staticShader->setMat4("projection", projection);
+		staticShader->setMat4("view", view);
+
+		// Aplicamos transformaciones del modelo
+		glm::mat4 model = glm::mat4(1.0f);
+		model = glm::translate(model, glm::vec3(0.0f, 0.0f, 0.0f)); // translate it down so it's at the center of the scene
+		model = glm::rotate(model, glm::radians(-90.0f), glm::vec3(1.0f, 0.0f, 0.0f));
+		model = glm::scale(model, glm::vec3(1.0f, 1.0f, 1.0f));	// it's a bit too big for our scene, so scale it down
+		staticShader->setMat4("model", model);
+
+		object->Draw(*staticShader);
+	}
+
+	glUseProgram(0);
+
+	// objeto estático (cubemap)
+	{
+		// Activamos el shader del plano
+		staticShader->use();
+
+		// Aplicamos transformaciones de proyección y cámara (si las hubiera)
+		staticShader->setMat4("projection", projection);
+		staticShader->setMat4("view", view);
+
+		// Aplicamos transformaciones del modelo
+		glm::mat4 model = glm::mat4(1.0f);
+		model = glm::translate(model, glm::vec3(0.0f, 0.0f, 0.0f)); // translate it down so it's at the center of the scene
+		model = glm::rotate(model, glm::radians(-90.0f), glm::vec3(1.0f, 0.0f, 0.0f));
+		model = glm::scale(model, glm::vec3(100.0f, 100.0f, 100.0f));	// it's a bit too big for our scene, so scale it down
+		staticShader->setMat4("model", model);
+
+		cubeenv->Draw(*staticShader);
+
+	}
+
+	glUseProgram(0);
+	
+	// glfw: swap buffers 
+	glfwSwapBuffers(window);
+	glfwPollEvents();
+
+	return true;
+}
+
+// Procesamos entradas del teclado
+void processInput(GLFWwindow* window)
+{
+	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+		glfwSetWindowShouldClose(window, true);
+
+	if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS)
+		camera.ProcessKeyboard(FORWARD, deltaTime);
+	if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS)
+		camera.ProcessKeyboard(BACKWARD, deltaTime);
+	if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS)
+		camera.ProcessKeyboard(LEFT, deltaTime);
+	if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS)
+		camera.ProcessKeyboard(RIGHT, deltaTime);
+	if (glfwGetKey(window, GLFW_KEY_M) == GLFW_PRESS)
+		glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+	if (glfwGetKey(window, GLFW_KEY_N) == GLFW_PRESS)
+		glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
+	if (glfwGetKey(window, GLFW_KEY_B) == GLFW_PRESS)
+		glPolygonMode(GL_FRONT_AND_BACK, GL_POINT);
+
+	// Character movement
+	if (glfwGetKey(window, GLFW_KEY_UP) == GLFW_PRESS) {
+	}
+	if (glfwGetKey(window, GLFW_KEY_DOWN) == GLFW_PRESS) {
+	}
+	if (glfwGetKey(window, GLFW_KEY_LEFT) == GLFW_PRESS) {
+	}
+	if (glfwGetKey(window, GLFW_KEY_RIGHT) == GLFW_PRESS) {
+	}
+
+	if (glfwGetKey(window, GLFW_KEY_I) == GLFW_PRESS)
+	{
+	}
+	if (glfwGetKey(window, GLFW_KEY_K) == GLFW_PRESS)
+	{
+	}
+		
+	
+}
+
+// glfw: Actualizamos el puerto de vista si hay cambios del tamaño
+// de la ventana
+void framebuffer_size_callback(GLFWwindow* window, int width, int height)
+{
+	glViewport(0, 0, width, height);
+}
+
+// glfw: Callback del movimiento y eventos del mouse
+void mouse_callback(GLFWwindow* window, double xpos, double ypos)
+{
+	if (firstMouse)
+	{
+		lastX = (float)xpos;
+		lastY = (float)ypos;
+		firstMouse = false;
+	}
+
+	float xoffset = (float)xpos - lastX;
+	float yoffset = lastY - (float)ypos; 
+
+	lastX = (float)xpos;
+	lastY = (float)ypos;
+
+	camera.ProcessMouseMovement(xoffset, yoffset);
+}
+
+// glfw: Complemento para el movimiento y eventos del mouse
+void scroll_callback(GLFWwindow* window, double xoffset, double yoffset)
+{
+	camera.ProcessMouseScroll((float)yoffset);
+}
